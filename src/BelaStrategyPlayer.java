0c3df0942343530dda2df7b8d6bced8f63ee695a
Mateskoda <skodamate93@gmail.com>
2020-11-27 13:07:40 +0100

    int i

--- src/BelaStrategyPlayer.java
+++ src/BelaStrategyPlayer.java
@@ -2,6 +2,7 @@ public class BelaStrategyPlayer extends Strategy{
 
     public BelaStrategyPlayer(String name, int startingCapital, Strategy strategy) {
         super(name, startingCapital, strategy);
+        int i;
     }
 
     @Override
