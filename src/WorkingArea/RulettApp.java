c8c8e932ccce1a71b9739132250e30dea46327a6
poliphonalj <peteri@t-online.hu>
2020-12-01 08:15:03 +0100

    ugyesen mukodik a jatek resz, meg a beviteleket kell levizsgalni es a szimulaciot megirni

--- src/WorkingArea/RulettApp.java
+++ src/WorkingArea/RulettApp.java
@@ -3,15 +3,6 @@
 // (powered by FernFlower decompiler)
 //
 
-
-/*
-elvileg nem szamitja ki az actual capital, sum rewards ertekekt, ugy tunik hogy csak vesziteni tud........de azt jol
-csak az odds okra mukodik eddig
-        a userinterface vegen lehet beadni a mezonyeremenyeket reprezentalo tomboket
-*/
-
-
-
 package WorkingArea;
 
 import java.util.ArrayList;
@@ -35,103 +26,77 @@ public class RulettApp {
     public static void main(String[] args) {
         new RulettApp();
         ui = new UserInterface();
-        me=new Player("",0);
+        me = new Player(ui.getNameOfPlayer(), ui.getStartingCapital());
     }
 
     public static void userChoosedSimulation() {
+   // code here
+
+
+
+
     }
 
     public static void userChoosedToPlay() {
-        expectedNums =ui.getNumBet();
-        actualBet=ui.getBetAmount();
-        ui.getRangedBet();
-
-
-        if(me.getName()==null){
-            me=new Player(ui.getNameOfPlayer(),ui.getStartingCapital());
-            System.out.println("wewwewewewe");
-        }
-
-        //itt kell megadni a fogadasi szamokat egy tombben
-
-        if(expectedNums.length==0){                      //ha egy szamra fogadunk akkor egy egy elemu int tombot adunk at a setbetnumbers()nek
+        expectedNums = ui.getNumBet();
+        actualBet = ui.getBetAmount();
+        if (expectedNums.length == 0) {                      //ha egy szamra fogadunk akkor egy egy elemu int tombot adunk at a setbetnumbers()nek
             me.setBetNumbers(ui.getRangedBet());
-        }
-        else{
+        } else {
             me.setBetNumbers(expectedNums);         //egy tombot agunk at ha tobb mezore fogadunk
         }
         me.setActuelBet(actualBet);  //tet osszege
         players.clear();
         players.add(me);
-        //a simulate xturnnek kell:
+
         simulateXTurn(1);
+        int sum = ui.getStartingCapital();
+        ui.printResult(winnerNumber, ui.getNameOfPlayer(), me.getActuelBet(), sum + me.getActualCapital(), me.getActualCapital());
+        players.clear();
     }
 
     public static void simulateXTurn(int x) {
-        for(int i = 0; i < x; ++i) {
+        for (int i = 0; i < x; ++i) {
             oneTurn();
-            System.out.println("winnerNumber :" + winnerNumber);
-            Iterator var2 = players.iterator();
-
-            while(var2.hasNext()) {
-
-
-                Player player = (Player)var2.next();
-                int sumOfRewards=player.getActualCapital() - player.getStartingCapital();
-                ui.printResult(winnerNumber,player.getName(),player.getActuelBet(),player.getActualCapital(),sumOfRewards);
-
-
-
-               // System.out.println("Name " + );
-                System.out.println("ActuelBet " + player.getActuelBet());
-                System.out.println("ActualCapital " + player.getActualCapital());
-                System.out.println("SumOfRewards " + (player.getActualCapital() - player.getStartingCapital()));
-                System.out.println();
-            }
-
-            System.out.println();
         }
-
     }
 
+
     public void simulate10Turn() {
-        for(int i = 0; i < 10; ++i) {
+        for (int i = 0; i < 10; ++i) {
             oneTurn();
             System.out.println("winnerNumber :" + winnerNumber);
             Iterator var2 = players.iterator();
 
-            while(var2.hasNext()) {
-                Player player = (Player)var2.next();
+            while (var2.hasNext()) {
+                Player player = (Player) var2.next();
                 System.out.println(player.getActuelBet());
                 System.out.println(player.getActualCapital());
             }
         }
-
     }
 
     public void simulate100Turn() {
-        for(int i = 0; i < 100; ++i) {
+        for (int i = 0; i < 100; ++i) {
             oneTurn();
             System.out.println(winnerNumber + "winnerNumber");
             Iterator var2 = players.iterator();
 
-            while(var2.hasNext()) {
-                Player player = (Player)var2.next();
+            while (var2.hasNext()) {
+                Player player = (Player) var2.next();
                 System.out.println(player.getActuelBet());
                 System.out.println(player.getActualCapital());
             }
         }
-
     }
 
     public static void oneTurn() {
         Iterator var0 = players.iterator();
 
-        while(var0.hasNext()) {
-            Player player = (Player)var0.next();
+        while (var0.hasNext()) {
+            Player player = (Player) var0.next();
             player.takeABet();
         }
-
         winnerNumber = spinTheWheel();
         winnersReward();
         losersReward();
@@ -142,36 +107,31 @@ public class RulettApp {
     }
 
     public static void winnersReward() {
-      for(Player p:players){
-          for(int i=0;i<p.getBetNumbers().length;i++){
-              if(p.getBetNumbers()[i]==winnerNumber){
-                  int reward=p.getActualCapital()*p.getWinnersMultiplier();
-                  p.setActualCapital(p.getActualCapital()+reward);
-                  actualWinners.add(p);
-                  break;
-              }
-          }
-      }
-    }
-
-
-// hol teszi bele a gyoztest az actualwinnerbe
-
-    public static void losersReward() {
-        Iterator var0 = players.iterator();
-
-        while(var0.hasNext()) {
-            Player player = (Player)var0.next();
-            if (!actualWinners.contains(player)) {
-                player.setActualCapital(player.getActualCapital() - player.getActuelBet());
-                int i = player.getActualCapital();
-                double pr = player.getPreviousBet();
-                int j = player.getActuelBet();
-                boolean var6 = true;
+        int sum;
+        for (Player p : players) {
+            for (int i = 0; i < ui.getRangedBet().length; i++) {
+                if (ui.getRangedBet()[i] == winnerNumber) {
+                    actualWinners.add(p);
+                    if(ui.getRangedBet().length!=1){
+                        sum = p.getActualCapital() + p.getActuelBet();
+                    }
+                    else{
+                        sum=p.getActualCapital() + p.getActuelBet()*5;
+                    }
+                    p.setActualCapital(sum);
+                    break;
+                }
             }
         }
+    }
 
-        actualWinners = new ArrayList();
+    public static void losersReward() {
+        for (Player player : players) {
+            if (!(actualWinners.contains(player))) {
+                player.setActualCapital(player.getActualCapital() - player.getActuelBet());
+            }
+        }
+        actualWinners.clear();
     }
 
     public int getMinBet() {
@@ -181,6 +141,4 @@ public class RulettApp {
     public static int getMaxBet() {
         return maxBet;
     }
-
-
 }
